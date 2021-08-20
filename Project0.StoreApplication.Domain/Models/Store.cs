6e9f4fc3828af32d8ce08387ff7fa252422286d1
StevenGregoryRevature <steven.gregory@revature.net>
2021-08-20 10:04:31 -0400

    created some files for project0

---
+++ Project0.StoreApplication.Domain/Models/Store.cs
@@ -0,0 +1,12 @@
+namespace Project0.StoreApplication.Domain.Models
+{
+    public class Store
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
